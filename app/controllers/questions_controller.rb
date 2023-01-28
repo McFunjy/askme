10e9c40994be9629cd874c9ffb129bd952c20243
McFunjy <barminov_01@mail.ru>
2023-01-28 14:42:06 +0300

    Add partials, helpers and notices

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,25 +3,26 @@ class QuestionsController < ApplicationController
   def create
     @question = Question.create(question_params)
 
-    redirect_to @question
+    redirect_to @question, notice: 'Новый вопрос создан!'
   end
 
   def update
     @question.update(question_params)
 
-    redirect_to question_path(@question)
+    redirect_to question_path(@question), notice: 'Вопрос изменен!'
   end
 
   def destroy
     @question.destroy
 
-    redirect_to questions_path
+    redirect_to questions_path, notice: 'Вопрос удален!'
   end
 
   def show
   end
 
   def index
+    @question = Question.new
     @questions = Question.all
   end
 
