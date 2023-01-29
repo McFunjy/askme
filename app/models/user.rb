63513ee60b485610c69d033cc9270977b7a3059d
McFunjy <barminov_01@mail.ru>
2023-01-30 00:39:03 +0300

    Refactor user validation and add indexes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
     has_secure_password
 
-    before_save :downcase_nickname
+    before_validation :downcase_nickname, :downcase_email
 
     validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/ }
     validates :nickname, uniqueness: true, length: { maximum: 40 }, format: { with: /\A\w+\z/ }
 
+    private
+    
     def downcase_nickname
       nickname.downcase!
+    end
+
+    def downcase_email
+      email.downcase!
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_120151) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_212950) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -27,6 +27,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_120151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
 end
